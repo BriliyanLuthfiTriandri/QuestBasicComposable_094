fbe51b7fbd3c06c2edbbfe9d4b260b3d206cfb39
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-10-18 16:45:46 +0700

    menambahkan setContent dan ComposableLayoutTheme untuk tema dari tampilan

--- app/src/main/java/com/example/composablelayout/MainActivity.kt
+++ app/src/main/java/com/example/composablelayout/MainActivity.kt
@@ -31,5 +31,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        setContent {
+            ComposableLayoutTheme {
+                }
+            }
+        }
     }
 }
